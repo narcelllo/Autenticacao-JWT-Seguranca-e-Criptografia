3d675d7061a06a44c3bf32c443c8fe97a5264c3a
Marcello Magno <marcello_magno@hotmail.com>
2025-03-06 14:17:33 -0300

    config starting project

--- example_jnt.py
+++ example_jnt.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import jwt
 from datetime import timedelta, datetime, timezone
 
@@ -8,7 +8,8 @@ app = Flask(__name__)
 def login():
     token = jwt.encode(
         payload={
-            'exp': datetime.now(timezone.utc) + timedelta(minutes=1)
+            'exp': datetime.now(timezone.utc) + timedelta(minutes=1),
+            "email": "emailteste@.com"
         },
         key="minhaChave",
         algorithm="HS256"
@@ -18,7 +19,17 @@ def login():
 
 @app.route("/secret", methods=["POST"])
 def secret():
+    raw_token = request.headers.get("Authorization")
+    token = raw_token.split()[1]
+    try:
+        token_information = jwt.decode(token, key="minhaChave", algorithms="HS256")
+        print(token_information["email"])
+    except Exception as exception:
+        return jsonify({"error": str(exception)}), 404
+
+    
+
     return jsonify({"meu": "segredo"}), 200
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=3000)
+    app.run(host="0.0.0.0", port=3000, debug=True)
