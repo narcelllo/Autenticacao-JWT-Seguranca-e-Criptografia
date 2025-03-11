f4a616ef2ff1923b4e13f56625b160b123a9fcc8
Marcello Magno <marcello_magno@hotmail.com>
2025-03-11 11:49:22 -0300

    adding testes in user_repository

--- src/models/repositories/user_repository_test.py
+++ src/models/repositories/user_repository_test.py
@@ -1,23 +1,67 @@
-import pytest
-from .user_repository import UserRepository
 from src.models.settings.db_connection_handler import db_connection_handler
+from .user_repository import UserRepository
+from unittest.mock import Mock
 
-@pytest.mark.skip("Insert in DB")
-def test_repository():
-    db_connection_handler.connect()
-    conn = db_connection_handler.get_connection()
-    repo = UserRepository(conn)
+class MockCursor:
+    def __init__(self) -> None:
+        self.execute = Mock()
+        self.fetchone = Mock()
 
-    username = "NameTest2"
-    password = "password1235"
+class MockConnection:
+    def __init__(self) -> None:
+        self.cursor = Mock(return_value=MockCursor())
+        self.commit = Mock()
 
+def test_repository_user():
+    username = "name"
+    password = "heuh"
+
+    mock_connection = MockConnection()
+    repo = UserRepository(mock_connection)
     repo.registry_user(username, password)
-    user = repo.get_user_by_username(username)
+
+    cursor = mock_connection.cursor.return_value
     print()
-    print(user)
-    #print(user[0])
-    #print(user[1])
-    repo.edit_balance(user[0], 7894.56)
-    user1 = repo.get_user_by_username(username)
+    print(cursor.execute.call_args[0])
+
+    assert "INSERT INTO users" in cursor.execute.call_args[0][0]
+    assert "(username, password, balance)" in cursor.execute.call_args[0][0]
+    assert "VALUES" in cursor.execute.call_args[0][0]
+    assert "(?, ?, ?)" in cursor.execute.call_args[0][0]
+    assert cursor.execute.call_args[0][1] == (username, password, 0)
+
+def test_edit_balance():
+    balance = 123.65
+    user_id = 2
+
+    mock_connection = MockConnection()
+    repo = UserRepository(mock_connection)
+
+    repo.edit_balance(balance, user_id)
+
+    cursor = mock_connection.cursor.return_value
     print()
-    print(user1)
+    print(cursor.execute.call_args[0])
+
+    assert "UPDATE users" in cursor.execute.call_args[0][0]
+    assert "SET balance = ?" in cursor.execute.call_args[0][0]
+    assert "WHERE id = ?" in cursor.execute.call_args[0][0]
+    assert cursor.execute.call_args[0][1] == (user_id, balance)
+
+def test_get_user_by_username():
+    username = "username"
+
+    mock_connection = MockConnection()
+    repo = UserRepository(mock_connection)
+
+    repo.get_user_by_username(username)
+
+    cursor = mock_connection.cursor.return_value
+    print()
+    print(cursor.execute.call_args[0])
+
+    assert "SELECT id, username, password" in cursor.execute.call_args[0][0]
+    assert "FROM users" in cursor.execute.call_args[0][0]
+    assert "WHERE username = ?" in cursor.execute.call_args[0][0]
+    assert cursor.execute.call_args[0][1] == (username,)
+
