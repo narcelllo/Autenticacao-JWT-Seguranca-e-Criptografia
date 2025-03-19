7adb8749c1743145611ff19070ad996c7bbea7eb
Marcello Magno <marcello_magno@hotmail.com>
2025-03-19 19:23:30 -0300

    Implementing middlewares with auth_verify

--- src/main/routes/bank_account_routes.py
+++ src/main/routes/bank_account_routes.py
@@ -1,5 +1,6 @@
 from flask import jsonify, Blueprint, request
 from src.views.http_types.http_request import HttpRequest
+from src.main.middlewares.auth_jwt import auth_jwt_verify
 
 from src.main.composer.user_register_composer import user_register_composer
 from src.main.composer.login_creator_composer import login_creator_composer
@@ -21,6 +22,9 @@ def create_login():
 
 @bank_routes_bp.route("/bank/balance/<user_id>", methods={"PATCH"})
 def edit_balanc(user_id):
-    http_request = HttpRequest(body=request.json, params={"user_id": user_id} )
+    token_information = auth_jwt_verify()
+    
+    http_request = HttpRequest(body=request.json, params={"user_id": user_id}, token_infos = token_information, headers = request.headers)
+
     http_response = balance_editor_composer().handle(http_request)
     return jsonify(http_response.body), http_response.status_code   
